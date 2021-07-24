f060730c56714b29560d89bc40b5bd578b58cab8
Osujdenie <osujdenie@yandex.ru>
2021-07-24 17:36:21 +0300

    bubble class

--- main.py
+++ main.py
@@ -1,25 +1,34 @@
 from tkinter import *
 import random
-h, w = 500, 600
-RandW = random.randrange(0,w)
-RandH = random.randrange(0,h)
+root=Tk()
+h, w = 400, 600
+MyCanvas = Canvas(root, height=h, width=w)
+MyCanvas.pack()
 
-class SoapBubbles():
-    def __init__(self, master):
-        self.root = master
+class App():
+    def __init__(self):
         root.title('Soap Bubble Of Pain')
-        self.MyCanvas = Canvas(self.root, height=h, width=w)
-        self.MyCanvas.pack()
-        self.position = [w / 2, h / 2]
-        self.Bubble = self.MyCanvas.create_oval(RandW - 20, RandH - 20, RandW + 20,
-                                                RandH + 20, fill="blue", activefill='red')
+        #for i in range(10):
+        a = SoapBubble()
+        b = SoapBubble()
+
+class SoapBubble():
+    def __init__(self):
+        RandW = random.randrange(0, w)
+        RandH = random.randrange(0, h)
+        self.bubble = MyCanvas.create_oval(RandW - 20, RandH - 20, RandW + 20,
+                                                RandH + 20, fill='blue', activefill='red')
+        self.mooving = [3, 3]
         self.MoveBubble()
 
     def MoveBubble(self):
-        self.MyCanvas.move(self.Bubble, 1, 1)
-        if self.MyCanvas.coords(self.Bubble)[2] < h - 20:
-            root.after(10, self.MoveBubble)
+        coords = MyCanvas.coords(self.bubble)
+        if 600 - 40 <= coords[0] or coords[0] <= 0:
+            self.mooving[0] *= -1
+        if 400 - 40 <= coords[1] or coords[1] <= 0:
+            self.mooving[1] *= -1
+        MyCanvas.move(self.bubble, self.mooving[0], self.mooving[1])
+        root.after(20, self.MoveBubble)
 
-root=Tk()
-SoapBubbles(root)
+App()
 root.mainloop()
